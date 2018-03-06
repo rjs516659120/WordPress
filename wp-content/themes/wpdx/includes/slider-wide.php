fadd860bfb034c04c36f8ab29b25b7180c51ecaa
rjs <516659120@qq.com>
2018-03-06 15:38:57 +0800

    121
    4545

--- wp-content/themes/wpdx/includes/slider-wide.php
+++ wp-content/themes/wpdx/includes/slider-wide.php
@@ -101,7 +101,7 @@ else $pager = 'false';
 <?php endif; wp_reset_query();?>
 <?php else: ?>
     <div class="row-fluid">
-        <div style="width: 856px;float: left;">
+        <div class="span12 ">
             <div id="home-slider" class="widget-box">
                 <div class="widget-content">
                     <ul class="bxslider">
